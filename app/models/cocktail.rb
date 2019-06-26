a598ebbcd6063a12eb610b6b38b476b326a4464d
Timothy Angus <timangus321@gmail.com>
2019-06-26 11:20:37 +0200

    pushing so I can set up a branch

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-  # mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
 
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
